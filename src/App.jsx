bbce9c1f0b806a8f810bbe1f3cd47a14bda5effc
RenishReddy <alavalarenish@gmail.com>
2025-03-25 14:42:37 +0530

    3rd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-// import "./App.css";
+import "./App.css";
 import { BrowserRouter, Link,Routes,Route } from "react-router-dom";
 import App1 from "./components/App1";
 import App2 from "./components/App2";
 import App3 from "./components/App3";
 import App4 from "./components/App4";
-import App5 from "./components/App5"
+import App5 from "./components/App5";
 import App6 from "./components/App6";
 import App7 from "./components/App7";
 function App() {
