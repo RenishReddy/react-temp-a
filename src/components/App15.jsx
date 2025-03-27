3cee014071e468c5e9eef4a1aa7a6168832cfea1
RenishReddy <alavalarenish@gmail.com>
2025-03-27 15:03:52 +0530

    wwwwd

---
+++ src/components/App15.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaHome } from "react-icons/fa";
+export default function App15() {
+    return (
+      <div>
+          <h3>This is App15</h3>
+      <div>
+      <FaHome size={15} />
+      <label>Home</label>
+      </div>
+      </div>
+    );
+}
