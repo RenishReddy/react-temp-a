b938fe7bfe208fa50c0b5d680bf069abe486f86c
RenishReddy <alavalarenish@gmail.com>
2025-03-27 12:01:28 +0530

    wdwa

--- src/components/App11.jsx
+++ src/components/App11.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { useState, useEffect, useRef } from "react";
-export default function App10() {
-    return(
-        <div>
-            <h3>This is App11</h3>
-            <button>Goto App5</button>
-        </div>
-    )
+import { useNavigate,Link } from "react-router-dom";
+export default function App11() {
+  const Navigate = useNavigate();
+  const handleSubmit = () => {
+    Navigate("/app5");
+  };
+  return (
+    <div>
+      <h3>This is App11</h3>
+      <button onClick={handleSubmit}>Goto App5</button>
+      <p>
+        <Link to="/app5">App5</Link>
+      </p>
+    </div>
+  );
 }
