df2a34784b919361fa28f581227513bb349cf3ec
RenishReddy <alavalarenish@gmail.com>
2025-03-27 11:44:10 +0530

    dwdw

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import App7 from "./components/App7";
 import App8 from "./components/App8";
 import App9 from "./components/App9";
 import App10 from "./components/App10";
+import App11 from "./components/App11";
 function App() {
   return (
     <BrowserRouter>
@@ -29,6 +30,7 @@ function App() {
         <Link to="app8">App8</Link>
         <Link to="app9">App9</Link>
         <Link to="app10">App10</Link>
+        <Link to="app11">App11</Link>
 
 
 
@@ -47,6 +49,7 @@ function App() {
           <Route path="app8" element={<App8 />} />
           <Route path="app9" element={<App9 />} />
           <Route path="app10" element={<App10 />} />
+          <Route path="app11" element={<App11 />} />
 
 
         </Routes>
---
+++ src/components/App11.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+export default function App10() {
+    return(
+        <div>
+            <h3>This is App11</h3>
+            <button>Goto App5</button>
+        </div>
+    )
+}
